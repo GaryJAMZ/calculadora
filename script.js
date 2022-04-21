a75ebb2208afeeba2ae9927da49fdeb0acf35e8b
Antonio Garibay <GaribayJAMZXN@outlook.com>
2022-04-21 13:35:24 -0500

    operaciones agregadas

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let operador = document.getElementById("operador");
 let resultado;
 let res = document.getElementById("output");
 let operacion;
+let vacio = document.getElementById("vacio");
 function sumas(){
     operador.innerHTML = "+";
     operacion = 1;
@@ -13,14 +14,21 @@ function restas(){
     operacion = 2;
 }
 function multiplicaciones(){
-    operador.innerHTML = "x";
+    operador.innerHTML = "X";
     operacion = 3;
 }
 function divisiones(){
     operador.innerHTML = "/";
     operacion = 4;
 }
-
+function raiz(){
+    operador.innerHTML = "&#8730;";
+    operacion = 5;
+}
+function exponente(){
+    operador.innerHTML = "X<sup> x</sup>";
+    operacion = 6;
+}
 function resultados(){
     switch(operacion){
         case 1:
@@ -38,6 +46,24 @@ function resultados(){
         case 4:
             resultado = (parseInt($(a).val())) / (parseInt($(b).val()));
             res.innerHTML = resultado;
+            break;
+        case 5:
+            if($(a).val() == ""){
+                resultado = Math.sqrt(parseInt($(b).val()));
+                res.innerHTML = resultado;
+            }else{
+                resultado = Math.pow(parseInt($(b).val()),1/parseInt($(a).val()));
+                res.innerHTML = resultado;
+            }
+            break;
+        case 6:
+            if($(b).val() == ""){
+                resultado = Math.pow(parseInt($(a).val()),2);
+                res.innerHTML = resultado;
+            }else{
+                resultado = Math.pow(parseInt($(a).val()),parseInt($(b).val()));
+                res.innerHTML = resultado;
+            }
             break;
         default:
             alert("Seleccione una operacion a ejecutar");
